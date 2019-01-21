c3bd85c12c3fc69707894667b547e267506b87b2
Jonathan Asias <jasiasp@ucentral.edu.co>
2019-01-21 14:39:39 -0500

    AI_MELI_MOD1_ALIMENTAR_EL_OJO FIXED

--- Assets/Recursos/MELI/AI_MELI_MOD1_ALIMENTAR_EL_OJO/Scripts/ActivityScore.cs
+++ Assets/Recursos/MELI/AI_MELI_MOD1_ALIMENTAR_EL_OJO/Scripts/ActivityScore.cs
@@ -5,10 +5,8 @@ using Resource.LIBRO_C.AI_MELI_MOD1_ANIMALES_EN_LA_MIRA.Scripts.AI_MELI_MOD1_ANI
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace Recursos.MELI.AI_MELI_MOD1_ALIMENTAR_EL_OJO.Scripts
-{
-    public class ActivityScore : MonoBehaviour
-    {
+namespace Recursos.MELI.AI_MELI_MOD1_ALIMENTAR_EL_OJO.Scripts {
+    public class ActivityScore : MonoBehaviour {
         //Array para guardar los objetos
         [Header("Objetos Dulces:")] public GameObject[] ArrayDulces;
 
@@ -20,48 +18,63 @@ namespace Recursos.MELI.AI_MELI_MOD1_ALIMENTAR_EL_OJO.Scripts
         [Header("Administrador de audios")] [SerializeField]
         public FXAudio _FxAudio;
 
-        [SerializeField] public Transform _piso; 
+        [SerializeField] public Transform _piso;
         public int Intentos, Aciertos;
 
         [SerializeField] private NavegationManager _navegationManager;
-        
+
         public Text AciertoText, IntentosText;
 
         public bool NotHaveRandom;
 
         private List<GameObject> ListaRandom = new List<GameObject>(); // Lista para guardar el objeto
 
+
         private int correctas, incorrectas = 0;
 
         [SerializeField] private Text _textoCorrecto, _textoIncorrecto;
 
+        List<Vector3> positions = new List<Vector3>();
 
-        public FXAudio FxAudio
-        {
+        public FXAudio FxAudio {
             get => _FxAudio;
             set => _FxAudio = value;
         }
 
-        public void Pause()
-        {
+        public void Pause() {
             Time.timeScale = 0;
             _personaje.SetActive(false);
-            
         }
 
-        public void Resume()
-        {
+        public void Resume() {
             Time.timeScale = 1;
             _personaje.SetActive(true);
         }
 
 
-//        private void OnDisable()
-//        {
-//            Aciertos = incorrectas = correctas = Intentos = 0;
-//            ScoreManager.ResetScore();
-//            
+        private void SavePosition() {
+            foreach (var elem in ArrayDulces) {
+                positions.Add(elem.transform.localPosition);
+            }
+        }
+
+//        private void getPositions() {
+//            foreach (var posicionDulces in positions) {
+//                foreach (var _ElemDulces in ArrayDulces) {
+//                    _ElemDulces.transform.position = posicionDulces;
+//                }
+//            }
 //        }
+
+        private void Start() {
+            SavePosition();
+        }
+
+
+        private void OnDisable() {
+            Time.timeScale = 1;
+            _personaje.SetActive(true);
+        }
 
 //		public void RandomElementos()
 //		{
@@ -81,54 +94,46 @@ namespace Recursos.MELI.AI_MELI_MOD1_ALIMENTAR_EL_OJO.Scripts
 //			}
 //		}
 
-        public void Calificar(bool Answer)
-        {
+        public void Calificar(bool Answer) {
             //_FxAudio.PlayAudio(Answer ? 2 : 1);
-            if (Answer)
-            {
+            if (Answer) {
                 _FxAudio.PlayAudio(2);
                 correctas++;
                 _textoCorrecto.text = correctas.ToString();
                 ScoreManager.IncreaseScore();
             }
-            else
-            {
+            else {
                 _FxAudio.PlayAudio(1);
                 incorrectas++;
                 _textoIncorrecto.text = incorrectas.ToString();
             }
-            
-            AsignarTexto(correctas,incorrectas);
+
+            AsignarTexto(correctas, incorrectas);
         }
 
-        public void AsignarTexto(int correctas, int incorrectas)
-        {
-            if (correctas == Aciertos)
-            {
+        public void AsignarTexto(int correctas, int incorrectas) {
+            if (correctas == Aciertos) {
                 //Time.timeScale = 0f;
                 _navegationManager.Forward(2);
             }
-            if (incorrectas == Intentos)
-            {    
+
+            if (incorrectas == Intentos) {
                 //Time.timeScale = 0f;
                 _navegationManager.Forward(2);
             }
         }
 
-        public void ResetVariables()
-        {
+        public void ResetVariables() {
             incorrectas = correctas = 0;
             _textoCorrecto.text = "0";
             _textoIncorrecto.text = "0";
-            
+
             ScoreManager.ResetScore();
 
-            foreach (var dulces in ArrayDulces)
-            {
-                dulces.SetActive(true);
-                dulces.transform.SetPositionAndRotation(_piso.position,Quaternion.identity);
+            for (int i = 0; i < positions.Count; i++) {
+                ArrayDulces[i].transform.localPosition = positions[i];
+                ArrayDulces[i].SetActive(true);
             }
-            
         }
     }
 }
--- Assets/Recursos/MELI/AI_MELI_MOD1_ALIMENTAR_EL_OJO/Scripts/Piso.cs
+++ Assets/Recursos/MELI/AI_MELI_MOD1_ALIMENTAR_EL_OJO/Scripts/Piso.cs
@@ -11,15 +11,36 @@ public class Piso : MonoBehaviour
 
     public Transform _limite1;
     public Transform _limite2;
+    private float x,y;
 
+    
     public float _massMin, _massMax, _gravityMin, _gravityMax;
     private IEnumerator coroutine;
 
     private void OnTriggerExit2D(Collider2D other)
     {
+    
         Vector3 pos = _techo.transform.position;
-        pos.x = Random.Range(_limite1.transform.position.x, _limite2.transform.position.x);
-        pos.y += Random.Range(-20f, 20f);
+        x = Random.Range(0f,1f);
+        if (x>0 && x<0.3)
+        {
+            pos.x = 750;
+        }
+        else
+        {
+            if (x>=0.3 && x <0.6)
+            {
+                pos.x = 825;
+            }
+            else
+            {
+                pos.x = 900;
+            }
+        }
+
+        //pos.x = x < 0.5f ? 750 : 900;
+        y = Random.Range(0f,1f);
+        pos.y += Random.Range(100f, 200f);
 
         StartCoroutine(WaitAndPrint(2.0f, pos, other));
     }
@@ -29,10 +50,10 @@ public class Piso : MonoBehaviour
         yield return new WaitForSeconds(waitTime);
         other.transform.SetPositionAndRotation(pos, Quaternion.identity);
         other.gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-        other.gameObject.GetComponent<Rigidbody2D>().mass =
-            Random.Range(_massMin, _massMax); //Asigna un vlaor aleatorio para la masa del objeto
-        other.gameObject.GetComponent<Rigidbody2D>().gravityScale =
-            Random.Range(_gravityMin, _gravityMax); //Asigna un vlaor aleatorio para la masa del objeto
-        //Debug.Log("llamado");
+        other.gameObject.GetComponent<Rigidbody2D>().mass = 4;
+            //Random.Range(_massMin, _massMax); //Asigna un vlaor aleatorio para la masa del objeto
+        other.gameObject.GetComponent<Rigidbody2D>().gravityScale = (float) 0.25;
+            //Random.Range(_gravityMin, _gravityMax); //Asigna un vlaor aleatorio para la masa del objeto
+            //Debug.Log("llamado");
     }
 }
